b807cead498633270e0428b5fd155055f6b01c88
sameetrathod84-ops <sameetrathod84@gmail.com>
2026-02-18 15:38:29 +0530

    Add files via upload

---
+++ pgm_list.py
@@ -0,0 +1,19 @@
+# creat a list
+numbers=[5,2,9,1,7]
+
+# add elements
+numbers.append(10)
+
+# insert elemens
+numbers.insert(2,6)
+
+# remove elements
+numbers.remove(1)
+
+#sort list
+numbers.sort()
+
+#reverse list
+numbers.reverse()
+
+print("final list",numbers)
